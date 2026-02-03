63e32d7b8ea01ed4318635e5890b2a186a5bc263
Nathan <suporteforlucas@hotmail.com>
2026-02-03 16:11:39 +0000

    Codigo Xadrez Nivel Novato finalizado

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,40 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    // INICIO DO PROGRAMA - TEMA 4 - NIVEL NOVATO //
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    // Torre - for; Bispo - While; Rainha - do-while
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    // Torre
+    printf("--- Movimento da Torre ---\n");
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+    for (int contador_torre = 0; contador_torre < 5; contador_torre++ ) {
+        printf ("Direita\n");
+    }
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
+    // Bispo
+    printf("--- Movimento da Bispo ---\n");
 
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+    int contador_bispo = 0;
+
+    while (contador_bispo < 5) {
+        printf ("Cima, Direita\n");
+        contador_bispo++ ;
+    }
+
+    // Rainha
+    printf("--- Movimento da Rainha ---\n");
+
+    int contador_rainha = 0 ;
+
+    do {
+        printf("Esquerda\n");
+        contador_rainha++;
+    } while (contador_rainha < 8);
+
+    
+    // FIM DO PROGRAMA - TEMA 4 - NIVEL NOVATO //
 
     return 0;
 }
